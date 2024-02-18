448060311a7b1799c0ace5cf3606b3f2062647fc
Valentin Belyaev <chuvalik.work@gmail.com>
2024-02-18 13:24:27 +0300

    update project structure

---
+++ src/agent.py
@@ -0,0 +1,65 @@
+import os
+
+from dotenv import load_dotenv
+from langchain import hub
+from langchain.agents import AgentExecutor
+from langchain.agents.format_scratchpad import format_log_to_str
+from langchain.agents.output_parsers import ReActSingleInputOutputParser
+from langchain.memory import ConversationBufferMemory
+from langchain.tools.render import render_text_description
+from langchain_community.tools.ddg_search import DuckDuckGoSearchRun
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_BASE_URL = os.getenv("OPENAI_BASE_URL")
+
+
+def _get_llm():
+    llm = ChatOpenAI(
+        openai_api_key=OPENAI_API_KEY,
+        base_url=OPENAI_BASE_URL,
+        model_name='mistralai/mixtral-8x7b-instruct',
+    )
+    llm_with_stop = llm.bind(stop=["\nObservation"])
+
+    return llm_with_stop
+
+
+def _get_prompt(tools):
+    agent_prompt = hub.pull("hwchase17/react-chat")
+
+    prompt = agent_prompt.partial(
+        tools=render_text_description(tools),
+        tool_names=", ".join([t.name for t in tools]),
+    )
+
+    return prompt
+
+
+def get_agent():
+    tools = [DuckDuckGoSearchRun()]
+
+    llm = _get_llm()
+    prompt = _get_prompt(tools)
+
+    agent = (
+            {
+                "input": lambda x: x["input"],
+                "agent_scratchpad": lambda x: format_log_to_str(x["intermediate_steps"]),
+                "chat_history": lambda x: x["chat_history"],
+            }
+            | prompt
+            | llm
+            | ReActSingleInputOutputParser()
+    )
+
+    agent_executor = AgentExecutor(
+        agent=agent,
+        tools=tools,
+        memory=ConversationBufferMemory(memory_key="chat_history"),
+        handle_parsing_errors=True
+    )
+
+    return agent_executor
--- src/main.py
+++ src/main.py
@@ -1,72 +1,9 @@
-import os
-
 import streamlit as st
-from dotenv import load_dotenv
-from langchain import hub
-from langchain.agents import AgentExecutor
-from langchain.agents.format_scratchpad import format_log_to_str
-from langchain.agents.output_parsers import ReActSingleInputOutputParser
-from langchain.memory import ConversationBufferMemory
-from langchain.tools.render import render_text_description
-from langchain_community.tools.ddg_search import DuckDuckGoSearchRun
-from langchain_openai import ChatOpenAI
 
-load_dotenv()
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-OPENAI_BASE_URL = os.getenv("OPENAI_BASE_URL")
+from agent import get_agent
 
 
-def handle_user_input(prompt):
-    response = st.session_state.conversation.invoke({"input": prompt})['output']
-    st.markdown(response)
-
-    st.session_state.messages.append(
-        {"role": "assistant", "content": response}
-    )
-
-
-def get_agent():
-    llm = ChatOpenAI(
-        openai_api_key=OPENAI_API_KEY,
-        base_url=OPENAI_BASE_URL,
-        model_name='mistralai/mixtral-8x7b-instruct',
-    )
-    llm_with_stop = llm.bind(stop=["\nObservation"])
-
-    tools = [DuckDuckGoSearchRun()]
-
-    agent_prompt = hub.pull("hwchase17/react-chat")
-
-    prompt = agent_prompt.partial(
-        tools=render_text_description(tools),
-        tool_names=", ".join([t.name for t in tools]),
-    )
-
-    agent = (
-            {
-                "input": lambda x: x["input"],
-                "agent_scratchpad": lambda x: format_log_to_str(x["intermediate_steps"]),
-                "chat_history": lambda x: x["chat_history"],
-            }
-            | prompt
-            | llm_with_stop
-            | ReActSingleInputOutputParser()
-    )
-
-    agent_executor = AgentExecutor(
-        agent=agent,
-        tools=tools,
-        memory=ConversationBufferMemory(memory_key="chat_history"),
-        handle_parsing_errors=True
-    )
-
-    return agent_executor
-
-
-def main():
-    st.title("Chat with Mixtral")
-
+def init_session_state():
     if "messages" not in st.session_state:
         st.session_state.messages = [
             {"role": "assistant", "content": "How can I help you?"}
@@ -75,19 +12,39 @@ def main():
     if "conversation" not in st.session_state:
         st.session_state.conversation = get_agent()
 
+
+def handle_user_input(prompt):
+    st.session_state.messages.append(
+        {'role': 'user', 'content': prompt}
+    )
+
+    with st.chat_message('user'):
+        st.write(prompt)
+    with st.chat_message('assistant'):
+        response = st.session_state.conversation.invoke({"input": prompt})['output']
+        st.markdown(response)
+
+        st.session_state.messages.append(
+            {"role": "assistant", "content": response}
+        )
+
+
+def main():
+    st.set_page_config(
+        page_title="Chat with Mixtral",
+        page_icon=":parrot:"
+    )
+    st.title("Chat with Mixtral")
+
+    init_session_state()
+
     for message in st.session_state.messages:
         with st.chat_message(message['role']):
             st.write(message['content'])
 
-    if prompt := st.chat_input():
-        st.session_state.messages.append(
-            {'role': 'user', 'content': prompt}
-        )
-
-        with st.chat_message('user'):
-            st.write(prompt)
-        with st.chat_message('assistant'):
-            handle_user_input(prompt)
+    prompt = st.chat_input()
+    if prompt:
+        handle_user_input(prompt)
 
 
 if __name__ == "__main__":
