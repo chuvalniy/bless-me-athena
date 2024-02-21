3ed594210c52ae7964c17d8baca7adb5cde073ca
Valentin Belyaev <chuvalik.work@gmail.com>
2024-02-21 07:33:55 +0300

    Add arXiv tools, connect llm to langsmith

--- src/model/agent.py
+++ src/model/agent.py
@@ -8,12 +8,17 @@ from langchain.agents.output_parsers import ReActSingleInputOutputParser
 from langchain.memory import ConversationBufferMemory
 from langchain.tools.render import render_text_description
 from langchain_community.tools.ddg_search import DuckDuckGoSearchRun
+from langchain_community.tools.arxiv.tool import ArxivQueryRun
 from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENAI_BASE_URL = os.getenv("OPENAI_BASE_URL")
+
+os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
+os.environ['LANGCHAIN_TRACING_V2'] = os.getenv('LANGCHAIN_TRACING_V2')
+os.environ['LANGCHAIN_PROJECT'] = os.getenv('LANGCHAIN_PROJECT')
 
 
 def _get_llm():
@@ -41,7 +46,8 @@ def _get_prompt(tools):
 
 def get_agent():
     tools = [
-        DuckDuckGoSearchRun()
+        DuckDuckGoSearchRun(),
+        ArxivQueryRun()
     ]
 
     llm = _get_llm()
